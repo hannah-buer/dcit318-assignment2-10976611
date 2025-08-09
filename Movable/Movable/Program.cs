c3754ff94fc1374225cc0a428083616e88fca3d8
hannah-buer <103133203+hannah-buer@users.noreply.github.com>
2025-08-09 12:15:47 +0000

    Interfaces completed

---
+++ Movable/Movable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instances of Car and Bicycle
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            // Calling the Move() method on each instance
+            car.Move();
+            bicycle.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
