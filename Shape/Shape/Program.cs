7983d628786a06b7a0b10e14fd46d89c3a5c7e73
hannah-buer <103133203+hannah-buer@users.noreply.github.com>
2025-08-09 12:14:30 +0000

    Abstract Classes and Methods completed

---
+++ Shape/Shape/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating instances of Circle and Rectangle
+            Circle circle = new Circle(5.0);
+            Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+            // Displaying their areas
+            Console.WriteLine($"Area of the circle: {circle.GetArea()}");      
+            Console.WriteLine($"Area of the rectangle: {rectangle.GetArea()}");
+
+            Console.ReadLine();
+        }
+    }
+}
