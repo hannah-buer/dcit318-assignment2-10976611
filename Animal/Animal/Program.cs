958efcaae5351252c4280fe29aa777b6e1e8e965
hannah-buer <103133203+hannah-buer@users.noreply.github.com>
2025-08-09 12:13:21 +0000

    Inheritance and Method Overriding completed

---
+++ Animal/Animal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating instances
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Calling MakeSound method on each instance
+            genericAnimal.MakeSound(); 
+            dog.MakeSound();           
+            cat.MakeSound();
+
+            Console.ReadLine(); 
+        }
+    }
+}
